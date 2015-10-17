a6d4bd876a82dd51ba028017fb9c276f0deb242a
aleth <aleth@instantbird.org>
2015-10-17 18:35:43 +0200

    Find kpsewhich on OSX 10.11

--- kpsewhich.py
+++ kpsewhich.py
@@ -3,9 +3,9 @@ from subprocess import Popen, PIPE
 def kpsewhich(filename, file_format=None):
     try:
         if file_format is None or type(file_format) != type(''):
-            p = Popen(['/usr/texbin/kpsewhich', filename], stdout=PIPE, stdin=PIPE)
+            p = Popen(['/Library/TeX/texbin/kpsewhich', filename], stdout=PIPE, stdin=PIPE)
         else:
-            p = Popen(['/usr/texbin/kpsewhich', '-format=%s' % (file_format), filename], stdout=PIPE, stdin=PIPE)
+            p = Popen(['/Library/TeX/kpsewhich', '-format=%s' % (file_format), filename], stdout=PIPE, stdin=PIPE)
         path = p.communicate()[0].rstrip()
         if p.returncode == 0:
             return path
